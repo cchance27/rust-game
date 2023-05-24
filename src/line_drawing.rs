8b04c4b4d2c321bffbd84ce79ec5507e8714bef8
Chris Chance <chrischance@outlook.com>
2023-05-24 19:32:30 -0400

    no more lines

--- src/line_drawing.rs
+++ src/line_drawing.rs
@@ -15,33 +15,3 @@ impl From<Square> for Polyline {
         }
     }
 }
-
-#[derive(Debug, Clone)]
-pub struct LineList {
-    pub lines: Vec<(Vec3, Vec3)>,
-}
-impl From<LineList> for Mesh {
-    fn from(line: LineList) -> Self {
-        let mut mesh = Mesh::new(PrimitiveTopology::LineList);
-
-        let vertices: Vec<_> = line.lines.into_iter().flat_map(|(a, b)| [a, b]).collect();
-        mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, vertices);
-        mesh
-    }
-}
-
-/// A list of points that will have a line drawn between each consecutive points
-#[derive(Debug, Clone)]
-pub struct LineStrip {
-    pub points: Vec<Vec3>,
-}
-impl From<LineStrip> for Mesh {
-    fn from(line: LineStrip) -> Self {
-        // This tells wgpu that the positions are a list of points
-        // where a line will be drawn between each consecutive point
-        let mut mesh = Mesh::new(PrimitiveTopology::LineStrip);
-
-        mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, line.points);
-        mesh
-    }
-}
