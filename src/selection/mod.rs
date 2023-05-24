db3e62b55c35e6f6319a27b3ce094e0bfdce493a
Chris Chance <chrischance@outlook.com>
2023-05-24 18:19:22 -0400

    gatekeep selection, cleanup deps

--- src/selection/mod.rs
+++ src/selection/mod.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, input::mouse::{MouseMotion, MouseButtonInput}};
 
 pub mod components;
 mod resources;
@@ -14,7 +14,8 @@ impl Plugin for SelectionPlugin {
     fn build(&self, app: &mut App) {
         app        
             .insert_resource(Selecting::default())
-            .add_system(handle_mouse_input_selection)
+            .add_system(handle_mouse_input_selection
+                .run_if(on_event::<MouseMotion>().or_else(on_event::<MouseButtonInput>())))
             .add_system(draw_selection_indicator)
             .add_system(draw_selection_box);
     }
--- src/selection/systems.rs
+++ src/selection/systems.rs
@@ -26,11 +26,11 @@ pub fn handle_mouse_input_selection(
     let Ok(window) = windows.get_single() else { return; };
     let Some(cursor_position) = window.cursor_position() else { return; };
     let Ok((camera, camera_location)) = camera.get_single() else { return; };
+    let Some(hit_entity) = screen_ray_to_entity(&camera, &rapier_context, &camera_location, cursor_position) else { return; };
 
     // We just started selecting something with left mouse button
     // Find what we're pointing at and set it as being the start of our selection.
     if mouse_btn.just_pressed(MouseButton::Left) {
-        let Some(hit_entity) = screen_ray_to_entity(&camera, &rapier_context, &camera_location, cursor_position) else { return; };
         if selectable.contains(hit_entity.0) {
             // We started on something non-selectable
             commands.insert_resource(Selecting { first_entity: Some(hit_entity.0), first_click: hit_entity.1, last_click: hit_entity.1, ..default() });
@@ -42,7 +42,6 @@ pub fn handle_mouse_input_selection(
 
     // We're likely dragging, check if we've moved if we have update our selecting state.
     if mouse_btn.pressed(MouseButton::Left) {
-        let Some(hit_entity) = screen_ray_to_entity(&camera, &rapier_context, &camera_location, cursor_position) else { return; };
         if selecting.first_click.distance(hit_entity.1) > 0.0 {
             selecting.last_click = hit_entity.1;
         }
@@ -58,36 +57,33 @@ pub fn handle_mouse_input_selection(
                 if let Some(entity) = commands.get_entity(old_selection) { entity.despawn_recursive() }
             }) 
         }
-        
-        // Check where we were pointing where we let go of the click or drag
-        if let Some(hit_entity) = screen_ray_to_entity(&camera, &rapier_context, &camera_location, cursor_position) {
-            // Is the first item we were on the same as the last item we were over (single click)
-            if selecting.first_entity.is_some() && selecting.first_entity.unwrap() == hit_entity.0 { 
-                // 1 Item so just select it, if it exists still                
-                if let Some(mut entity_to_select) = commands.get_entity(hit_entity.0) {
+                
+        // Is the first item we were on the same as the last item we were over (single click)
+        if selecting.first_entity.is_some() && selecting.first_entity.unwrap() == hit_entity.0 { 
+            // 1 Item so just select it, if it exists still                
+            if let Some(mut entity_to_select) = commands.get_entity(hit_entity.0) {
+                entity_to_select.insert(PendingSelection);
+            }
+        } else {
+            // MultiSect all in the box
+            selectable.iter().filter(|(_e, gt)| {
+                let tf = gt.translation();
+                let within_x = (selecting.first_click.x < tf.x && selecting.last_click.x > tf.x) || (selecting.last_click.x < tf.x && selecting.first_click.x > tf.x);
+                let within_z =  (selecting.first_click.z < tf.z && selecting.last_click.z > tf.z) || (selecting.last_click.z < tf.z && selecting.first_click.z > tf.z);
+                within_x && within_z
+            }).for_each(|valid|{
+                if let Some(mut entity_to_select) = commands.get_entity(valid.0) {
                     entity_to_select.insert(PendingSelection);
                 }
-            } else {
-                // MultiSect all in the box
-                selectable.iter().filter(|(_e, gt)| {
-                    let tf = gt.translation();
-                    let within_x = (selecting.first_click.x < tf.x && selecting.last_click.x > tf.x) || (selecting.last_click.x < tf.x && selecting.first_click.x > tf.x);
-                    let within_z =  (selecting.first_click.z < tf.z && selecting.last_click.z > tf.z) || (selecting.last_click.z < tf.z && selecting.first_click.z > tf.z);
+            });
+        }
 
-                    within_x && within_z
-                }).for_each(|valid|{
-                    if let Some(mut entity_to_select) = commands.get_entity(valid.0) {
-                        entity_to_select.insert(PendingSelection);
-                    }
-                });
-            }
+        // Drop our picking box entity, and then reset our selecting resource
+        if let Some(pickingbox) = selecting.picking_box {
+            commands.get_entity(pickingbox).unwrap().despawn_recursive();
+        }
 
-            // Drop our picking box entity, and then reset our selecting resource
-            if let Some(pickingbox) = selecting.picking_box {
-                commands.get_entity(pickingbox).unwrap().despawn_recursive();
-            }
-            commands.insert_resource(Selecting::default());
-        }  
+        commands.insert_resource(Selecting::default());
     }
 }
 
